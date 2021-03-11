a7f47f79bdb6a063fd6e96a97844dbe365458c0c
fm mahmud <mahmudmsmr1@gmail.com>
2021-03-11 09:08:33 +0600

    Moshiur project

---
+++ resources/js/main.js
@@ -0,0 +1,9 @@
+ $(document).ready(function(){
+//     var mixer = mixitup('.data-filter');
+
+  var mixer = mixitup(".container");
+
+
+ });
+
+
---
+++ zip/resources/js/main.js
@@ -0,0 +1,15 @@
+ $(document).ready(function(){
+//     var mixer = mixitup('.data-filter');
+
+
+var mixer = mixitup(containerEl, {
+    selectors: {
+        target: '.data-filter'
+    },
+    
+});
+
+
+ });
+
+
